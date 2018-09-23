1813fd1e50b5acaa16551ecac03cc14b0cdd28dc
Yassin <yasnarutoa@gmail.com>
2018-09-23 12:09:00 -0400

    initial commit

---
+++ index.js
@@ -0,0 +1,9 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+
+const mydiv = (<h1>
+this is now a header tagged
+</h1>);
+
+ReactDOM.render(mydiv, document.getElementById('app'));
